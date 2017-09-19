4ca674d7924b57a2ed6677e3850c7d9f60f0ef5d
Kimberly Bordon and Rakhee Gandhi <rakhee@>
2017-09-19 12:00:10 -0700

    Add submit listener event.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#sortingForm").submit(function(event) {
+    $("#quizResposes").show();
+    $("input:checkbox[name=survey-question]:checked").each(function() {
+      var 
+
+    }
+
+
+    event.preventDefault();
+  });
+});
